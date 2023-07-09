b54fdc4c5cc05731666813e1b4f3824a5948bf1e
SirichaiEiEi <108708170+SirichaiEiEi@users.noreply.github.com>
2023-07-10 01:25:31 +0700

    Add PlayerMove

---
+++ Assets/Scrip/Game/Player/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f; // ความเร็วในการเคลื่อนที่ของผู้เล่น
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+        rb.velocity = moveDirection * moveSpeed;
+    }
+}
